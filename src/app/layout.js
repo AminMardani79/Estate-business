c42539b3190b98c6bbdf37e93537f57c248d2a10
a.mardani <aminmardani1819@gmail.com>
2024-01-19 17:39:40 +0330

    Move layout component to its own file.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,13 @@
-import './globals.css'
-import { yekan } from '@/utilsfonts'
+import Layout from "@/layout/Layout";
+import "./globals.css";
+import { yekan } from "@/utilsfonts";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={yekan.className}>{children}</body>
+      <body className={yekan.className}>
+        <Layout>{children}</Layout>
+      </body>
     </html>
-  )
+  );
 }
