6f904256f7fe63d4d0d161b71f7d643dd1a259bf
a.mardani <aminmardani1819@gmail.com>
2024-02-10 19:58:02 +0330

    fix: import redirect

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,7 @@
 import DashboardSidebar from "@/layout/DashboardSidebar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 async function DashboardLayout({ children }) {
   const session = await getServerSession(authOptions);
